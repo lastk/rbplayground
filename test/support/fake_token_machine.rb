b2a1eaf433975dfdc020a9d629f127011643901d
Rafael Oliveira <rafael.leite.oliveira@gmail.com>
2016-05-28 23:57:29 -0300

    add a token constant to `FakeTokenMachine`

--- test/support/fake_token_machine.rb
+++ test/support/fake_token_machine.rb
@@ -1,6 +1,7 @@
 class FakeTokenMachine < TokenMachine
   EXPIRATION_DATE = DateTime.new(2020,01,01)
+  FAKE_TOKEN = 'ABCDEF'
   def new_token(valid_until=EXPIRATION_DATE)
-    Token.new('ABCDEF', valid_until)
+    Token.new(FAKE_TOKEN, valid_until)
   end
 end
